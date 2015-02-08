7a4f465ec4a6f3b9944419eb20a6a840cdea7da2
martincostello <martin@martincostello.com>
2015-02-08 20:22:57 +0000

    Switched back to using wp_title().

--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -30,7 +30,7 @@
     <meta property="og:description" content="<?php bloginfo('description'); ?>" />
     <meta property="og:locale" content="en_GB" />
     <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
-    <meta property="og:title" content="<?php the_title(); ?>" />
+    <meta property="og:title" content="<?php wp_title(''); ?>" />
     <meta property="og:type" content="article" />
     <meta property="og:url" content="" />
     <meta name="twitter:card" content="summary">
@@ -38,7 +38,7 @@
     <meta name="twitter:description" content="<?php bloginfo('description'); ?>">
     <meta name="twitter:domain" content="blog.martincostello.com" />
     <meta name="twitter:site" content="@martin_costello">
-    <meta name="twitter:title" content="<?php the_title(); ?>">
+    <meta name="twitter:title" content="<?php wp_title(''); ?>">
     <meta name="google-site-verification" content="ji6SNsPQEbNQmF252sQgQFswh-b6cDnNOa3AHvgo4J0" />
     <meta name="msvalidate.01" content="D6C2E7551C902F1A396D8564C6452930" />
     <title><?php wp_title( '|', true, 'right' ); ?></title>
