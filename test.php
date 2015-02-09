dd1bca72d0753ecdc2d38150876fd75d196cb616
martincostello <martin@martincostello.com>
2015-02-09 02:10:21 +0000

    Changed test.php to match example for connectivity to SQL Server.

--- test.php
+++ test.php
@@ -1,4 +1,20 @@
 <?php
 phpinfo();
-sqlsrv_client_info();
+?>
+<?php
+$serverName = "vuhhxypltc.database.windows.net,1433";
+$connOptions = array("UID"=>"wordpress_user@vuhhxypltc", "PWD"=>"?Oe8QeqKj2UWVWS");
+$conn = sqlsrv_connect( $serverName, $connOptions );
+
+if( $conn === false ) {
+    die( print_r( sqlsrv_errors(), true));
+}
+
+if( $client_info = sqlsrv_client_info( $conn)) {
+    foreach( $client_info as $key => $value) {
+        echo $key.": ".$value."<br />";
+    }
+} else {
+    echo "Error in retrieving client info.<br />";
+}
 ?>
