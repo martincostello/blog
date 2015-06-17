5e86a48a9fad1024a710a2375030ef35b690eac8
martincostello <martin@martincostello.com>
2015-06-17 14:44:33 +0100

    Remove smartypants from markdown.

--- config.rb
+++ config.rb
@@ -110,7 +110,7 @@ end
 
 activate :syntax
 set :markdown_engine, :redcarpet
-set :markdown, :fenced_code_blocks => true, :smartypants => true
+set :markdown, :fenced_code_blocks => true
 
 ##
 # Custom Configuration
