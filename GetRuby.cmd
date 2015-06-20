6583e93d07d839048405a0193058c3affe059a68
martincostello <martin@martincostello.com>
2015-06-20 16:21:33 +0100

    Normalize casing of echo.

--- GetRuby.cmd
+++ GetRuby.cmd
@@ -1,6 +1,6 @@
 REM Based on, and thanks to, https://github.com/shanselman/march-is-for-makers/blob/master/getruby.cmd
 
-@ECHO ON
+@echo ON
 
 REM Put Ruby in Path
 REM You can also use %TEMP% but it is cleared on site restart. Tools is persistent.
@@ -20,16 +20,16 @@ REM curl -o rubyrails193.zip http://dl.bintray.com/oneclick/rubyinstaller/ruby-2
 REM 64bit
 curl -o ruby215.zip http://dl.bintray.com/oneclick/rubyinstaller/ruby-2.1.5-x64-mingw32.7z?direct
 REM Azure puts 7zip here!
-EcHO START Unzipping Ruby
+echo START Unzipping Ruby
 d:\7zip\7za x -xr!*.ri -y ruby215.zip > rubyout
-EcHO DONE Unzipping Ruby
+echo DONE Unzipping Ruby
 
 REM Get DevKit to build Ruby native gems  
 REM If you don't need DevKit, rem this out.
 curl -o DevKit.zip http://cdn.rubyinstaller.org/archives/devkits/DevKit-mingw64-64-4.7.2-20130224-1432-sfx.exe
-EcHO START Unzipping DevKit
+echo START Unzipping DevKit
 d:\7zip\7za x -y -oDevKit DevKit.zip > devkitout
-EcHO DONE Unzipping DevKit
+echo DONE Unzipping DevKit
 
 REM Init DevKit
 ruby DevKit\dk.rb init
@@ -45,7 +45,7 @@ REM Update Gem223 until someone fixes the Ruby Windows installer https://github.
 curl -L -o update.gem https://github.com/rubygems/rubygems/releases/download/v2.2.3/rubygems-update-2.2.3.gem
 call gem install --local update.gem
 call update_rubygems --no-ri --no-rdoc > updaterubygemsout
-ECHO What's our new Rubygems version?
+echo What's our new Rubygems version?
 call gem --version
 call gem uninstall rubygems-update -x
 
@@ -59,7 +59,7 @@ cd
 
 call gem install bundler
 
-ECHO Bundler install (not update!)
+echo Bundler install (not update!)
 call bundle install
 
 cd %DEPLOYMENT_SOURCE%
