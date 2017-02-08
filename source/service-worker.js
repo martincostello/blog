95cc100d28b04521cfaa4147687fdf43fc9e5df9
martincostello <martin@martincostello.com>
2017-02-08 15:55:01 +0000

    Add service worker
    Add a basic service worker.

---
+++ source/service-worker.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+console.log("Started Service Worker.", self);
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open("blog.martincostello.com").then(function (cache) {
+            return cache.addAll([
+                "/"
+            ]);
+        }).then(function () {
+            return self.skipWaiting();
+        })
+    );
+    console.log("Installed Service Worker.");
+});
+
+self.addEventListener("activate", function (event) {
+    console.log("Activated Service Worker.");
+});
