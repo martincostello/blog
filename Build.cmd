09bbc382b947a678fd0a2c7db9ce8369a178cf97
martincostello <martin@martincostello.com>
2017-02-08 13:55:50 +0000

    Add branch name to footer GitHub link
    Add the name of the branch to the link in the footer on desktop.
    Add title attributes to footer links.

--- Build.cmd
+++ Build.cmd
@@ -3,4 +3,6 @@
 SET PATH=%PATH%;D:\home\site\deployments\tools\r\ruby-2.2.2-x64-mingw32\bin
 
 git rev-parse HEAD > version.txt
+git rev-parse --abbrev-ref HEAD > branch.txt
+
 bundle exec middleman build %*
