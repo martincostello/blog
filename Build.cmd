019d20b8c58711310c470e615b0e025d0f2f8cdd
martincostello <martin@martincostello.com>
2023-05-16 14:18:30 +0100

    Add build script
    Add a simple PowerShell build script.

--- Build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-SET PATH=%PATH%;D:\home\site\deployments\tools\r\ruby-2.2.2-x64-mingw32\bin
-
-git rev-parse HEAD > version.txt
-git rev-parse --abbrev-ref HEAD > branch.txt
-
-bundle exec middleman build %*
