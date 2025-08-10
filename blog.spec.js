01f9878dac7561c37bff05b1b481ebe984f6fd5f
martincostello <martin@martincostello.com>
2025-08-10 18:13:39 +0100

    Test archive page
    Ensure that all the pages in the archive load.

--- blog.spec.js
+++ blog.spec.js
@@ -16,3 +16,27 @@ test('about me link', async ({ page }) => {
 
   await expect(page.getByRole('heading', { name: 'About Me' })).toBeVisible();
 });
+
+test('archive links are valid', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('link', { name: 'Archive' }).click();
+
+  await expect(page).toHaveTitle(/Archive/);
+
+  const archiveLinks = await page.locator('a.archive-link');
+  await expect(archiveLinks).not.toHaveCount(0);
+
+  const articles = [];
+
+  for (const link of await archiveLinks.elementHandles()) {
+    articles.push(await link.getAttribute('href'));
+  }
+
+  for (const article of articles) {
+    await expect(async () => {
+      const response = await page.request.get(article);
+      expect(response.status()).toBe(200);
+    }).toPass();
+  }
+});
