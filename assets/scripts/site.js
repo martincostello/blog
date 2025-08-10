d5a88acbff23953236e9f136054884473cd0a17f
martincostello <martin@martincostello.com>
2025-08-10 14:52:54 +0100

    Rename variable
    Rename JavaScript variable.

--- assets/scripts/site.js
+++ assets/scripts/site.js
@@ -29,9 +29,9 @@ window.addEventListener('load', async () => {
 !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https'; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.defer = ""; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'twitter-wjs');
 
 // Google Analytics
-if (window.hugoSiteParams && window.hugoSiteParams.renderAnalytics) {
+if (window.siteParameters && window.siteParameters.renderAnalytics) {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', window.hugoSiteParams.analyticsId);
+    gtag('config', window.siteParameters.analyticsId);
 }
